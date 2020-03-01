df536bc66bff6433a427e5bbc1afcfe3fc2a02bc
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-01 20:10:37 +0300

    Basic House.

---
+++ Wild Tales/Assets/Codes/Follow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Follow : MonoBehaviour {
+    public Transform target;
+    public float smooth = 10f;
+
+    void LateUpdate() {
+        if (target != null && Vector2.Distance(target.position, transform.position) > 0.5) {
+            Vector3 new_position = Vector2.Lerp(transform.position, target.position, smooth * Time.fixedDeltaTime);
+            transform.position = new Vector3(new_position.x, new_position.y, transform.position.z);
+        }
+    }
+}
+
