58bc9c8255786fe0c845a19f89c656adfb5f665d
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-03 18:29:38 +0300

    Start Working On Obstacle.

---
+++ Wild Tales/Assets/Codes/Editor/MenuItems.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MenuItems {
+    [MenuItem("Tools/Add Obstacle")]
+    static void add_obstacle() {
+        foreach (var go in Selection.gameObjects) {
+            Debug.Log(go.name);
+        }
+    }
+}
