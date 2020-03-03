f8d3e3b24d515d51c40a11679e60be984e069050
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-03 08:34:05 +0300

    Enemys Talk To Themselfs.

--- Wild Tales/Assets/Codes/FSM/Enemy/EnemyIdle.cs
+++ Wild Tales/Assets/Codes/FSM/Enemy/EnemyIdle.cs
@@ -6,8 +6,14 @@ public class EnemyIdle : BasicFSM<Enemy> {
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         base.OnStateUpdate(animator, stateInfo, layerIndex);
         /* ============================================== */
-        if (ob.eye.see<Player>(LayerMask.GetMask("Top Layer"))) {
-            ob.player = ob.eye.see<Player>(LayerMask.NameToLayer("Top Layer"));
+        if (ob.eye.see<Player>(LayerMask.GetMask("Top Layer")))
+            ob.player = ob.eye.see<Player>(LayerMask.GetMask("Top Layer"));
+        /* ============================================== */
+        if (ob.player) {
+            foreach (var enemy in ob.eye.see_all<Enemy>(LayerMask.GetMask("Top Layer"))) {
+                if (!enemy.player)
+                    enemy.player = ob.player;
+            }
             ob.GetComponent<Animator>().SetTrigger("chase");
         }
     }
