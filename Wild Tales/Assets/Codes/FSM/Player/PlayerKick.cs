f8a8edbc11cc7030ca576ab6cd29154b94fdb781
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-06 23:55:22 +0300

    Hit Plus Projectile. Start Working.

---
+++ Wild Tales/Assets/Codes/FSM/Player/PlayerKick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerKick : BasicFSM<Player> {
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        /* ======================================================== */
+        if (ob.attack_area.overlap<Projectile>(LayerMask.GetMask("Floor Layer"))) {
+            ob.attack_area.overlap<Projectile>(LayerMask.GetMask("Floor Layer")).hit(-1 * ob.transform.up);
+        }
+    }
+}
---
+++ Wild Tales/Assets/Codes/Projectile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+    public void hit(Vector2 direction) {
+        Debug.Log("Hit");
+    }
+}
