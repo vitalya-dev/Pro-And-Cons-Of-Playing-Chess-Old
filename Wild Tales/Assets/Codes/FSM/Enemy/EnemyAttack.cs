63b007ea3d7c7ebed9758ff2351b52f3d3d524f7
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-07 16:24:30 +0300

    Quick Jab.

--- Wild Tales/Assets/Codes/FSM/Enemy/EnemyAttack.cs
+++ Wild Tales/Assets/Codes/FSM/Enemy/EnemyAttack.cs
@@ -5,10 +5,24 @@ public class EnemyAttack : BasicFSM<Enemy> {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         base.OnStateEnter(animator, stateInfo, layerIndex);
         /* ============================================== */
-        rb.MovePosition(rb.position + (Vector2)rb.transform.up * -1);
-        if (ob.attack_area.overlap<Player>(LayerMask.GetMask("Top Layer")))
+        if (ob.attack_area.overlap<Player>(LayerMask.GetMask("Top Layer"))) {
+            animator.SetFloat("attack speed", 1.0f);
             ob.attack_area.overlap<Player>(LayerMask.GetMask("Top Layer")).hit(-1 * ob.transform.up);
-        else if (ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer")))
+        }
+        else if (ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer"))) {
+            animator.SetFloat("attack speed", 5.0f);
             ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer")).hit(-1 * ob.transform.up);
+        }
+        else {
+            animator.SetFloat("attack speed", 5.0f);
+        }
+        rb.MovePosition(rb.position + (Vector2)rb.transform.up * -1);
+
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        /* ============================================== */
+        animator.SetFloat("attack speed", 1.0f);
     }
 }
--- Wild Tales/Assets/Codes/FSM/Player/PlayerAttack.cs
+++ Wild Tales/Assets/Codes/FSM/Player/PlayerAttack.cs
@@ -4,15 +4,27 @@ using System.Collections;
 public class PlayerAttack : BasicFSM<Player> {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         base.OnStateEnter(animator, stateInfo, layerIndex);
+        /* ============================================== */
         if (ob.attack_area.overlap<Enemy>(LayerMask.GetMask("Top Layer"))) {
+             animator.SetFloat("attack speed", 1.0f);
             ob.attack_area.overlap<Enemy>(LayerMask.GetMask("Top Layer")).hit(-1 * ob.transform.up);
         }
         else if (ob.attack_area.overlap<Envi>(LayerMask.GetMask("Top Layer"))) {
             ob.attack_area.overlap<Envi>(LayerMask.GetMask("Top Layer")).hit(-1 * ob.transform.up);
+            animator.SetFloat("attack speed", 2.0f);
         }
         else if (ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer"))) {
             ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer")).hit(-1 * ob.transform.up);
+            animator.SetFloat("attack speed", 2.0f);
+        }
+        else {
+            animator.SetFloat("attack speed", 2.0f);
         }
         rb.MovePosition(rb.position + (Vector2)rb.transform.up * -1);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        animator.SetFloat("attack speed", 1.0f);
     }
 }
