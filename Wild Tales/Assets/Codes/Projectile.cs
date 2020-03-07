c1c833e485497f83f3404237c03a28664030909a
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-07 09:15:42 +0300

    Projectile Have Fly State.

--- Wild Tales/Assets/Codes/Projectile.cs
+++ Wild Tales/Assets/Codes/Projectile.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
     public GameObject particle;
+    private bool fly = false;
 
     public void hit(Vector2 direction) {
         gameObject.layer = LayerMask.NameToLayer("Middle Layer");
         GetComponent<Rigidbody2D>().AddForce(direction * 25, ForceMode2D.Impulse);
+        /* ========================================================= */
+        fly = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        collision.gameObject.SendMessage("stun", SendMessageOptions.DontRequireReceiver);
-        /* ========================================================= */
-        GameObject.Instantiate(particle, transform.position, Quaternion.identity);
-        GameObject.Destroy(this.gameObject);
+        if (fly) {
+            collision.gameObject.SendMessage("stun", SendMessageOptions.DontRequireReceiver);
+            /* ========================================================= */
+            GameObject.Instantiate(particle, transform.position, Quaternion.identity);
+            GameObject.Destroy(this.gameObject);
+        }
     }
 }
