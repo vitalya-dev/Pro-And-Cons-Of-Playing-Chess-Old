b9e4fa80f585b36278ea28783cf9031cd1931e7c
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-07 08:05:31 +0300

    Projectile Kicked.

--- Wild Tales/Assets/Codes/FSM/Player/PlayerKick.cs
+++ Wild Tales/Assets/Codes/FSM/Player/PlayerKick.cs
@@ -5,8 +5,15 @@ public class PlayerKick : BasicFSM<Player> {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         base.OnStateEnter(animator, stateInfo, layerIndex);
         /* ======================================================== */
+        ob.GetComponent<Collider2D>().enabled = false;
+        /* ======================================================== */
         if (ob.attack_area.overlap<Projectile>(LayerMask.GetMask("Floor Layer"))) {
             ob.attack_area.overlap<Projectile>(LayerMask.GetMask("Floor Layer")).hit(-1 * ob.transform.up);
         }
+    }
+
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) {
+        ob.GetComponent<Collider2D>().enabled = true;
     }
 }
--- Wild Tales/Assets/Codes/Projectile.cs
+++ Wild Tales/Assets/Codes/Projectile.cs
@@ -2,7 +2,17 @@
 using System.Collections;
 
 public class Projectile : MonoBehaviour {
+    public GameObject particle;
+
     public void hit(Vector2 direction) {
-        Debug.Log("Hit");
+        gameObject.layer = LayerMask.NameToLayer("Middle Layer");
+        GetComponent<Rigidbody2D>().AddForce(direction * 25, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        collision.gameObject.SendMessage("stun");
+        /* ========================================================= */
+        GameObject.Instantiate(particle, transform.position, Quaternion.identity);
+        GameObject.Destroy(this.gameObject);
     }
 }
