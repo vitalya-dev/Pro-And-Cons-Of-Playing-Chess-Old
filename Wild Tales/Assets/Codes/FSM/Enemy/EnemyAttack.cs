b11adb3f7a99efabb596fbdb73568eefe8f74681
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-08 07:35:49 +0300

    Little Bit Fix Of Quick Jab.

--- Wild Tales/Assets/Codes/FSM/Enemy/EnemyAttack.cs
+++ Wild Tales/Assets/Codes/FSM/Enemy/EnemyAttack.cs
@@ -6,18 +6,13 @@ public class EnemyAttack : BasicFSM<Enemy> {
         base.OnStateEnter(animator, stateInfo, layerIndex);
         /* ============================================== */
         if (ob.attack_area.overlap<Player>(LayerMask.GetMask("Top Layer"))) {
-            animator.SetFloat("attack speed", 1.0f);
             ob.attack_area.overlap<Player>(LayerMask.GetMask("Top Layer")).hit(-1 * ob.transform.up);
         }
         else if (ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer"))) {
             animator.SetFloat("attack speed", 5.0f);
             ob.attack_area.overlap<Envi>(LayerMask.GetMask("Middle Layer")).hit(-1 * ob.transform.up);
         }
-        else {
-            animator.SetFloat("attack speed", 5.0f);
-        }
         rb.MovePosition(rb.position + (Vector2)rb.transform.up * -1);
-
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
