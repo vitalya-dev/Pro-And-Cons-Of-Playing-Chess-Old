85d2415b1027969662ecea7fe34a7087078b7c07
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-14 08:45:24 +0300

    4 Building.

--- Wild Tales/Assets/Codes/Editor/MenuItems.cs
+++ Wild Tales/Assets/Codes/Editor/MenuItems.cs
@@ -14,6 +14,14 @@ public class MenuItems {
     static void snap() {
         foreach (var go in Selection.gameObjects) {
             go.transform.localPosition = new Vector3((int)go.transform.localPosition.x, (int)go.transform.localPosition.y, (int)go.transform.localPosition.z);
+            go.transform.localScale = new Vector3(Mathf.RoundToInt(go.transform.localScale.x), Mathf.RoundToInt(go.transform.localScale.y), Mathf.RoundToInt(go.transform.localScale.z));
         }
     }
+
+    [MenuItem("Tools/Select parent &w")]
+    static void select_parent() {
+        Selection.activeGameObject = Selection.activeGameObject.transform.parent.gameObject;
+
+    }
 }
+
