23eeb6b82760ea5aa5b124002f1db7a8ccff5eac
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-14 07:35:06 +0300

    Snapping.

--- Wild Tales/Assets/Codes/Editor/MenuItems.cs
+++ Wild Tales/Assets/Codes/Editor/MenuItems.cs
@@ -9,4 +9,11 @@ public class MenuItems {
             obstacle.transform.parent = go.transform;
         }
     }
+
+    [MenuItem("Tools/Snap")]
+    static void snap() {
+        foreach (var go in Selection.gameObjects) {
+            go.transform.localPosition = new Vector3((int)go.transform.localPosition.x, (int)go.transform.localPosition.y, (int)go.transform.localPosition.z);
+        }
+    }
 }
