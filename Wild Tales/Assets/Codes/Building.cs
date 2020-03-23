3517b8af55bec480da5f6ca139fee74d2d343267
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-23 09:30:27 +0300

    Start Working On Building Cleanup.

---
+++ Wild Tales/Assets/Codes/Building.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+    [HideInInspector]
+    public Area area;
+
+    // Use this for initialization
+    void Start() {
+        area = transform.Find("Area").GetComponent<Area>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (area.overlap<Enemy>(LayerMask.GetMask("Top Layer"))) {
+            Debug.Log("Still Have Enemy");
+        }
+    }
+}
