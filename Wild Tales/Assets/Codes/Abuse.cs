072c57763292bc53244fef37d387c6993d2aed9f
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-31 00:14:40 +0300

    Got Abuses

---
+++ Wild Tales/Assets/Codes/Abuse.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Abuse {
+    public static string[] nouns = {
+        "Kal",
+        "Otrebya",
+        "Ubliudki",
+        "Pidery",
+        "Osloyoabi",
+        "Vieblyadky",
+        "Goamiki",
+        "Sissski",
+        "Rozhie",
+        "Papiki",
+        "Vyrodki",
+        "Zaluupi",
+        "Zhopi"
+    };
+
+    public static string[] adj = {
+        "Konchenii",
+        "Ebyanii",
+        "Griaznyjie",
+        "Vonychiie",
+        "Ebuetcshayi",
+        "Huieavi",
+        "Dolbaonie",
+        "Zarazniye",
+        "Falshiviaie"
+    };
+
+    public static string make_small_abuse() {
+        if (Random.Range(0, 100) < 50)
+            return nouns[Random.Range(0, nouns.Length)] + " " + adj[Random.Range(0, adj.Length)];
+        else
+            return adj[Random.Range(0, adj.Length)] + " " + nouns[Random.Range(0, nouns.Length)];
+    }
+}
