e3acfc4f22848e4de892cc0e2a4d4cafa5dfbad6
Vitalya Dev <vitalya.dev@gmail.com>
2020-03-31 23:36:58 +0300

    Door Is Destroyed.

--- Wild Tales/Assets/Codes/Envi.cs
+++ Wild Tales/Assets/Codes/Envi.cs
@@ -14,6 +14,10 @@ public class Envi : MonoBehaviour {
         is_active = gameObject.activeSelf;
     }
 
+    public void hit() {
+        hit(Vector2.zero);
+    }
+
     public void hit(Vector2 direction) {
         GameObject.Instantiate(particle, transform.position, Quaternion.identity);
         gameObject.SetActive(false);
