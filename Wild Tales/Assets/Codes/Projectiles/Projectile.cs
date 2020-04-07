ddf961932513a22185c72f346faffbd509dd58a0
Vitalya Dev <vitalya.dev@gmail.com>
2020-04-07 08:53:01 +0300

    Projectile Is Fly.

--- Wild Tales/Assets/Codes/Projectiles/Projectile.cs
+++ Wild Tales/Assets/Codes/Projectiles/Projectile.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class Projectile : MonoBehaviour {
     public GameObject particle;
 
-    public void hit(Vector2 direction, float force ) {
-        
+    private Vector3 velocity;
+
+    public void hit(Vector2 direction, float force) {
+        transform.Translate(0, 0, -1);
+        velocity = direction * force;
     }
 
+    void Update() {
+        if (velocity.magnitude >= 1)
+            GetComponent<Physicsbody>().move_position(transform.position + velocity * Time.fixedDeltaTime, GetComponent<BoxCollider>());
 
+    }
 
     void on_collision(Collider collider) {
-        
+        GameObject.Instantiate(particle, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
     }
 
 }
