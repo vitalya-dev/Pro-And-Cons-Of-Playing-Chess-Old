15d5af6c31edf86fc6c3ab86f0f7757a1f6b3f5a
Vitalya Dev <vitalya.dev@gmail.com>
2020-04-14 08:40:35 +0300

    Working On Projectiles.

--- Wild Tales/Assets/Codes/Projectiles/Projectile.cs
+++ Wild Tales/Assets/Codes/Projectiles/Projectile.cs
@@ -8,16 +8,19 @@ public class Projectile : MonoBehaviour {
     private Vector3 velocity;
 
     public void hit(Vector3 direction, float force) {
-		transform.Translate(Vector3.up);
+		transform.Translate(Vector3.up * 0.1f);
 		velocity = direction * force;
     }
  
     void Update() {
 		transform.position += velocity * Time.deltaTime;
 		if (velocity.sqrMagnitude > 0 && GetComponent<Area>().overlap<Collider>()) {
+			GameObject.Instantiate(particle, transform.position, Quaternion.identity);
 			GameObject.Destroy(gameObject);
+			/* ====================================================== */
 			Debug.Log(GetComponent<Area>().overlap<BoxCollider>());
 		}
+
     }
 
 }
