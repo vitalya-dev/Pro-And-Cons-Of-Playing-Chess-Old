9b0d8403dc447eeb9fac77bb28fec2a15a10abdc
Vitalya Dev <vitalya.dev@gmail.com>
2020-04-18 09:07:38 +0300

    Chase Is Working.

--- Wild Tales/Assets/Codes/FSM/Enemy/EnemyChase.cs
+++ Wild Tales/Assets/Codes/FSM/Enemy/EnemyChase.cs
@@ -1,23 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyChase : BasicFSM<Enemy> {
-	NavMeshPath path;
+	Vector3[] corners;
 
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		base.OnStateEnter(animator, stateInfo, layerIndex);
 		/* ============================================== */
-		path = ob.calculate_path(ob.transform.position, ob.player.transform.position);
+		corners = ob.calculate_path(ob.transform.position, ob.player.transform.position).corners.Clone() as Vector3[];
 	}
 
-	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		base.OnStateUpdate(animator, stateInfo, layerIndex);
 		/* ============================================== */
-		for (int i = 0; i < path.corners.Length - 1; i++) {
-			Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+		if (corners.Length > 1) {
+			var d = Vector3.Scale(corners[1] - ob.transform.position, new Vector3(1, 0, 1));
+			if (d.magnitude > 0.1) {
+				ob.transform.position += Vector3.ClampMagnitude(d.normalized * ob.speed * Time.deltaTime, d.magnitude);
+			} else {
+				ob.transform.position = corners[1] + Vector3.Scale(ob.transform.position, new Vector3(0, 1, 0));
+				/********************* shifting *********************/
+				Array.Copy(corners, 1, corners, 0, corners.Length - 1);
+				Array.Resize(ref corners, corners.Length - 1);
+				/********************* shifting *********************/
+			}
+			/********************* debug *********************/
+			for (int i = 0; i < corners.Length - 1; i++) {
+				Debug.DrawLine(corners[i], corners[i + 1], Color.red);
+			}
+			/********************* debug *********************/
+		} else {
+			corners = ob.calculate_path(ob.transform.position, ob.player.transform.position).corners.Clone() as Vector3[];
 		}
 	}
 }
