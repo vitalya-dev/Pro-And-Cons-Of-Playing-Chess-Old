9142eaec7c216f22c5f189e19cb5e3597ec12bea
Vitalya Dev <vitalya.dev@gmail.com>
2020-04-18 16:38:49 +0300

    Working On Player Attack.

--- Wild Tales/Assets/Codes/FSM/BasicFSM.cs
+++ Wild Tales/Assets/Codes/FSM/BasicFSM.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class BasicFSM<T> : StateMachineBehaviour {
     [HideInInspector]
     public T ob;
-    public Physicsbody pb;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
         ob = animator.GetComponent<T>();
-        pb = animator.GetComponent<Physicsbody>();
     }
 }
