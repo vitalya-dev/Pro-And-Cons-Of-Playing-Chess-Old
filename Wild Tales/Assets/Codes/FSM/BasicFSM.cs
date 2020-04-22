93dc0d3cc4d97b847776061fd1b44f72d8eede3f
vitalya-dev <vitalya.dev@gmail.com>
2020-04-22 07:39:26 +0300

    Still Working On States Machines.

--- Wild Tales/Assets/Codes/FSM/BasicFSM.cs
+++ Wild Tales/Assets/Codes/FSM/BasicFSM.cs
@@ -9,11 +9,17 @@ public class BasicFSM<T> : StateMachineBehaviour {
 	public AI ai;
 	[HideInInspector]
 	public PhysicBody pb;
+	public string[] triggers;
 
 	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		ob = animator.GetComponent<T>();
 		ai = animator.GetComponent<AI>();
 		pb = animator.GetComponent<PhysicBody>();
+	}
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		foreach (var trigger in triggers)
+			animator.ResetTrigger(trigger);
 	}
 }
--- Wild Tales/Assets/Codes/FSM/Player/PlayerAttack.cs
+++ Wild Tales/Assets/Codes/FSM/Player/PlayerAttack.cs
@@ -22,6 +22,5 @@ public class PlayerAttack : BasicFSM<Player> {
 	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		base.OnStateExit(animator, stateInfo, layerIndex);
 		/* ============================================== */
-		animator.SetFloat("attack speed", 1.0f);
 	}
 }
