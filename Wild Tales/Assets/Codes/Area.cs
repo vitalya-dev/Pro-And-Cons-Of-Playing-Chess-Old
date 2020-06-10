21e9497eee98a15b2c9e70ae1019d826aaee8fda
vitalya-dev <vitalya.dev@gmail.com>
2020-06-10 09:29:20 +0300

    Time To Take A Bath.

--- Wild Tales/Assets/Codes/Area.cs
+++ Wild Tales/Assets/Codes/Area.cs
@@ -4,25 +4,24 @@ using UnityEditor;
 using UnityEngine;
 
 public class Area : MonoBehaviour {
-	public T overlap<T>() where T : class {
-		Collider[] colliders;
-		/* ============================================ */
-		if (GetComponent<BoxCollider>())
-			colliders = Physics.OverlapBox(
-				transform.position + GetComponent<BoxCollider>().center,
-				GetComponent<BoxCollider>().size / 2,
-				transform.rotation
-			);
-		else if (GetComponent<SphereCollider>())
-			colliders = Physics.OverlapSphere(transform.position, GetComponent<SphereCollider>().radius);
-		else
-			colliders = new Collider[0];
-		/* ============================================ */
-		foreach (var collider in colliders) {
-			if (collider.gameObject != gameObject && collider.GetComponent<T>() != null) {
-				return collider.GetComponent<T>();
-			}
-		}
-		return null;
-	}
+  public T overlap<T>() where T : class {
+    Collider[] colliders;
+    /* ============================================ */
+    if (GetComponent<BoxCollider>())
+      colliders = Physics.OverlapBox(transform.position + GetComponent<BoxCollider>().center,
+                                     GetComponent<BoxCollider>().size / 2,
+                                     transform.rotation
+                                     );
+    else if (GetComponent<SphereCollider>())
+      colliders = Physics.OverlapSphere(transform.position, GetComponent<SphereCollider>().radius);
+    else
+      colliders = new Collider[0];
+    /* ============================================ */
+    foreach (var collider in colliders) {
+      if (collider.gameObject != gameObject && collider.GetComponent<T>() != null) {
+        return collider.GetComponent<T>();
+      }
+    }
+    return null;
+  }
 }
