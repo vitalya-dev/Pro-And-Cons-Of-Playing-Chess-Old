2bc282ec984c4a79150635f916876f79c3796ff6
vitalya-dev <vitalya.dev@gmail.com>
2020-06-21 17:28:07 +0300

    Door Close Sound.

--- Wild Tales/Assets/Code/Door.cs
+++ Wild Tales/Assets/Code/Door.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Door : MonoBehaviour {
   public Vector3 opend = Vector3.right;
 
+  /* ===================================================== */
+  private bool is_open = false;
   public virtual void open() {
     transform.rotation *= Quaternion.FromToRotation(Vector3.forward, opend);
     opend *= -1;
     /* ===================================================== */
-    GameObject.Find("Door Open").GetComponent<AudioSource>().Play();
+    if (is_open) 
+      GameObject.Find("Door Close").GetComponent<AudioSource>().Play();
+    else
+      GameObject.Find("Door Open").GetComponent<AudioSource>().Play();
+    /* ===================================================== */
+    is_open = !is_open;
   }
+  /* ===================================================== */
 }
