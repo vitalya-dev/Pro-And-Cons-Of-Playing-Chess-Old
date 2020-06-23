c355f8596902e14ffaeb071e33ad6030cbf2c7b5
vitalya-dev <vitalya.dev@gmail.com>
2020-06-23 09:22:09 +0300

    Phone Is Ring.

--- Wild Tales/Assets/Code/Phone.cs
+++ Wild Tales/Assets/Code/Phone.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 [SelectionBase]
 public class Phone : MonoBehaviour {
   public void pickup() {
+    GameObject.Find("Phone Ring").GetComponent<AudioSource>().Stop();
     GameObject.Find("Phone Pickup").GetComponent<AudioSource>().Play();
   }
+
   public void hangup() {
     GameObject.Find("Phone Hangup").GetComponent<AudioSource>().Play();
   }
+
+  public void ring() {
+    GameObject.Find("Phone Ring").GetComponent<AudioSource>().loop = true;
+    GameObject.Find("Phone Ring").GetComponent<AudioSource>().Play();
+  }
+
 }
