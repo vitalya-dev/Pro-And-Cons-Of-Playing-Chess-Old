d2f5dc6e1c542c7374a17f6422a2a7faa87aff04
vitalya-dev <vitalya.dev@gmail.com>
2020-07-02 08:02:04 +0300

    Structure reworking.

---
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Scene_1/Code/Player.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace scene_1 {
+  [SelectionBase]
+  public class Player : MonoBehaviour {
+    public float speed;
+    /* ===================================================== */
+    private float time;
+    /* ===================================================== */
+    [HideInInspector]
+    public Vector3 axis = Vector3.zero;
+    /* ===================================================== */
+    PhysicBody pb;
+    Animator am;
+
+    void Awake() {
+      pb = GetComponent<PhysicBody>();
+      am = GetComponent<Animator>();
+    }
+
+
+    void Start() {
+      time = 0;
+      foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("SLEEP:");
+    }
+
+    void Update() {
+      GetComponent<PlayMakerFSM>().FsmVariables.GetFsmFloat("time").RawValue = time;
+      /* ===================================================== */
+      axis.x = Input.GetAxisRaw("Horizontal");
+      axis.z = Input.GetAxisRaw("Vertical");
+      GetComponent<PlayMakerFSM>().FsmVariables.GetFsmFloat("axis").RawValue = axis.magnitude;
+      /* ===================================================== */
+      if (Input.GetButtonDown("Fire1"))
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("FIRE1");
+      if (Input.GetButtonDown("Fire2"))
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("FIRE2");
+      if (Input.GetButtonDown("Fire3"))
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("FIRE3");
+    }
+
+    void wait() {
+      am.Play("Idle");
+    }
+
+
+    void say(string text) {
+      am.Play("Idle");
+      /* ===================================================== */
+      if (text == "#NOTHING") {
+        foreach (var t_o in GameObject.FindObjectsOfType<TMPro.TextMeshPro>())
+          if (t_o.name.StartsWith(GetInstanceID() + "_")) Destroy(t_o.gameObject);
+        /* ===================================================== */
+        GameObject.Find("Mist").GetComponent<SpriteRenderer>().enabled = false;
+        /* ===================================================== */
+        return;
+      }
+      /* ===================================================== */
+      foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("TALK");
+      /* ===================================================== */
+      GameObject text_object = Instantiate(Resources.Load("Etc/Text", typeof(GameObject))) as GameObject;
+      /* ===================================================== */
+      text_object.GetComponent<TMPro.TextMeshPro>().text = text;
+      /* ===================================================== */
+      text_object.transform.rotation = Quaternion.LookRotation(Vector3.down, Vector3.forward);
+      /* ===================================================== */
+      float width = Mathf.Min(16, text.Length);
+      float height = 0;
+      /* ===================================================== */
+      text_object.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+      height = text_object.GetComponent<TMPro.TextMeshPro>().GetPreferredValues().y;
+      text_object.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+      /* ===================================================== */
+      text_object.transform.position = Vector3.Scale(transform.position, new Vector3(1, 0, 1));
+      text_object.transform.position += Vector3.up * (Camera.main.transform.position.y - 1); 
+      /* ===================================================== */
+      text_object.name = GetInstanceID() + "_" + text;
+      /* ===================================================== */
+      GameObject.Find("Mist").GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void time_inc() {
+      time += 1;
+    }
+
+
+    public IEnumerator idle_state() {
+      /* ===================================================== */
+      am.Play("Idle");
+      /* ===================================================== */
+      while (true) {
+        /* ===================================================== */
+        Vector3 c_p = GameObject.FindObjectOfType<Crosshair>().transform.position;
+        Vector3 look = Vector3.Scale(c_p - transform.position, new Vector3(1, 0, 1));
+        transform.rotation = Quaternion.LookRotation(look);
+        /* ===================================================== */
+        yield return null;
+      }
+    }
+
+    public IEnumerator walk_state() {
+      /* ===================================================== */
+      am.Play("Walk");
+      /* ===================================================== */
+      while (true) {
+        Vector3 offset_x = Vector3.Scale(axis, new Vector3(1, 0, 0)) * speed * Time.deltaTime;
+        Vector3 offset_z = Vector3.Scale(axis, new Vector3(0, 0, 1)) * speed * Time.deltaTime;
+        pb.move_position(transform.position + offset_x);
+        pb.move_position(transform.position + offset_z);
+        /* ===================================================== */
+        Vector3 c_p = GameObject.FindObjectOfType<Crosshair>().transform.position;
+        Vector3 look = Vector3.Scale(c_p - transform.position, new Vector3(1, 0, 1));
+        transform.rotation = Quaternion.LookRotation(look);
+        /* ===================================================== */
+        yield return null;
+      }
+    }
+
+    void interact() {
+      am.Play("Interact");
+      /* ===================================================== */
+      if (face_to_and_touch_to<Exit>()) {
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("EXIT");
+      } else if (face_to_and_touch_to<Door>()) {
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("DOOR");
+      } else if (face_to_and_touch_to<Bath>()) {
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("BATH");
+      } else if (face_to_and_touch_to<Cupboard>()) {
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("CUPBOARD");
+      } else if (face_to_and_touch_to<Phone>()) {
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("PHONE");
+      } else if (face_to_and_touch_to<Chess>()) {
+        foreach (PlayMakerFSM fsm in GetComponents<PlayMakerFSM>()) fsm.SendEvent("CHESS");
+      }
+
+    }
+
+    void get_dressed() {
+      Cupboard cupboard = face_to_and_touch_to<Cupboard>();
+      /* ===================================================== */
+      Sprite old_player_shoulder = transform.Find("Shoulder Right").GetComponent<SpriteRenderer>().sprite;
+      Sprite old_player_leg = transform.Find("Leg Right").GetComponent<SpriteRenderer>().sprite;
+      Sprite old_player_body = transform.Find("Body 1").GetComponent<SpriteRenderer>().sprite;
+      /* ===================================================== */
+      transform.Find("Shoulder Right").GetComponent<SpriteRenderer>().sprite = cupboard.shoulder_sprite;
+      transform.Find("Shoulder Left").GetComponent<SpriteRenderer>().sprite = cupboard.shoulder_sprite;
+      transform.Find("Leg Right").GetComponent<SpriteRenderer>().sprite = cupboard.leg_sprite;
+      transform.Find("Leg Left").GetComponent<SpriteRenderer>().sprite = cupboard.leg_sprite;
+      transform.Find("Body 1").GetComponent<SpriteRenderer>().sprite = cupboard.body_sprite;
+      transform.Find("Body 2").GetComponent<SpriteRenderer>().sprite = cupboard.body_sprite;
+      transform.Find("Body 3").GetComponent<SpriteRenderer>().sprite = cupboard.body_sprite;
+      /* ===================================================== */
+      cupboard.shoulder_sprite = old_player_shoulder;
+      cupboard.leg_sprite = old_player_leg;
+      cupboard.body_sprite = old_player_body;
+    }
+
+    void open_door() {
+      am.Play("Kick");
+      face_to_and_touch_to<Door>().open();
+    }
+
+    void sit_at_chair_1() {
+      am.Play("Idle");
+      /* ===================================================== */
+      backup_transform();
+      /* ===================================================== */
+      transform.position = GameObject.FindObjectOfType<Chair>().transform.position;
+      transform.rotation = Quaternion.LookRotation(Vector3.right);    
+      /* ===================================================== */
+      GetComponent<PhysicBody>().enabled = false;
+    }
+
+    void sit_at_chair_2() {
+      am.Play("Idle");
+      /* ===================================================== */
+      restore_transform();
+      /* ===================================================== */
+      GetComponent<PhysicBody>().enabled = true;
+    }
+
+    void pickup_phone() {
+      am.Play("Idle");
+      face_to_and_touch_to<Phone>().pickup();
+    }
+
+    void hangup_phone() {
+      am.Play("Idle");
+      face_to_and_touch_to<Phone>().hangup();
+    }
+
+    void open_cupboard() {
+      am.Play("Idle");
+      face_to_and_touch_to<Cupboard>().open();
+    }
+
+    void change_graphic(string graphic) {
+      string[] graphic_2 = graphic.Split(',');
+      /* ===================================================== */
+      GameObject body = transform.Find(graphic_2[0]).gameObject;
+      Sprite sprite = Resources.Load<Sprite>("Graphics/Layers/" + graphic_2[1]);
+      /* ===================================================== */
+      body.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+
+    void rotate(string angles) {
+      string[] angles_3 = angles.Split(',');
+      /* ===================================================== */
+      float x = float.Parse(angles_3[0]);
+      float y = float.Parse(angles_3[1]);
+      float z = float.Parse(angles_3[2]);
+      /* ===================================================== */
+      transform.rotation *= Quaternion.Euler(x, y, z);
+    }
+
+
+    void fill_bath() {
+      face_to_and_touch_to<Bath>().fill();
+    }
+
+    void drain_bath() {
+      face_to_and_touch_to<Bath>().drain();
+    }
+
+    void pass_out() {
+      GameObject.Find("Black").GetComponent<SpriteRenderer>().enabled = true;
+      GameObject.Find("Man In Me").GetComponent<AudioSource>().Play();
+    }
+
+
+    void take_bath_1() {
+      am.Play("Idle");
+      /* ===================================================== */
+      backup_transform();
+      /* ===================================================== */
+      transform.position = face_to_and_touch_to<Bath>().transform.position;
+      transform.rotation = Quaternion.LookRotation(Vector3.up, Vector3.right);
+      transform.position += new Vector3(0, -0.475f, 0);
+      /* ===================================================== */
+      GetComponent<PhysicBody>().enabled = false;
+      /* ===================================================== */
+      GameObject.Find("Whistling").GetComponent<AudioSource>().Play();
+    }
+
+    void take_bath_2() {
+      am.Play("Idle");
+      /* ===================================================== */
+      restore_transform();
+      /* ===================================================== */
+      GetComponent<PhysicBody>().enabled = true;
+      /* ===================================================== */
+      GameObject.Find("Whistling").GetComponent<AudioSource>().Stop();
+    }
+
+    void sleep_1() {
+      am.Play("Sleep");
+      /* ===================================================== */
+      GameObject.Find("Starry Night").GetComponent<SpriteRenderer>().enabled = true;
+      /* ===================================================== */
+      backup_transform();
+      /* ===================================================== */
+      transform.rotation = Quaternion.LookRotation(Vector3.up, Vector3.forward);
+      /* ===================================================== */
+      transform.position = GameObject.FindObjectOfType<Bed>().transform.position;
+      transform.position = Vector3.Scale(transform.position, new Vector3(1, 0, 1));
+      transform.position += Vector3.up * (Camera.main.transform.position.y - 2); 
+      /* ===================================================== */
+      GetComponent<PhysicBody>().enabled = false;
+      /* ===================================================== */
+      Camera.main.orthographicSize = 1.5f;
+      Camera.main.transform.position = Vector3.Scale(transform.position, new Vector3(1, 0, 1));
+      Camera.main.transform.position += new Vector3(0, 20, 0);
+    }
+
+    void sleep_2() {
+      am.Play("Sleep");
+      /* ===================================================== */
+      transform.rotation = Quaternion.LookRotation(Vector3.right, Vector3.forward);
+      /* ===================================================== */
+      GameObject.Find("Peace Of Mind").GetComponent<AudioSource>().Play();
+    }
+
+    void sleep_3() {
+      am.Play("Sleep");
+      /* ===================================================== */
+      GameObject.Find("Starry Night").GetComponent<SpriteRenderer>().enabled = false;
+      /* ===================================================== */
+      Camera.main.orthographicSize = 6.5f;
+    }
+
+    void sleep_4() {
+      am.Play("Sleep");
+      /* ===================================================== */
+      restore_transform();
+      /* ===================================================== */
+      GameObject.Find("Stop The Music").GetComponent<AudioSource>().Play();
+      GameObject.Find("Peace Of Mind").GetComponent<AudioSource>().Stop();
+      /* ===================================================== */
+      GetComponent<PhysicBody>().enabled = true;
+    }
+
+
+
+    T seen<T>() where T : MonoBehaviour {
+      RaycastHit hit;
+      if (Physics.Raycast(transform.position, transform.forward, out hit, 2) && hit.collider.GetComponent<T>())
+        return hit.collider.GetComponent<T>();
+      return null;
+    }
+    
+  
+    T face_to_and_touch_to<T>() where T : MonoBehaviour {
+      BoxCollider bc = GetComponent<BoxCollider>();
+      /* ============================================ */
+      Collider[] colliders = Physics.OverlapBox(transform.position + bc.center, bc.size / 2);
+      /* ============================================ */
+      foreach (var collider in colliders)
+        if (collider.gameObject != gameObject && collider.GetComponent<T>() != null) {
+          if ((Vector3.Angle(transform.forward, collider.bounds.center - transform.position)) < 60)
+            return collider.GetComponent<T>();
+        }
+      /* ============================================ */
+      return null;
+    }
+
+
+    /* ============================================ */
+    Vector3 backup_position;
+    Quaternion  backup_rotation;
+
+    void backup_transform() {
+      backup_position = transform.position;
+      backup_rotation = transform.rotation;
+    }
+
+    void restore_transform() {
+      transform.position = backup_position;
+      transform.rotation = backup_rotation;
+    }
+    /* ============================================ */
+
+  }
+}
