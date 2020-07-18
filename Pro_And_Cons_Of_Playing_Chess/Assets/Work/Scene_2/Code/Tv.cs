4fb9bcb50bc8a9acef3cbd6a306dddb103939b1e
vitalya-dev <vitalya.dev@gmail.com>
2020-07-18 09:14:37 +0300

    Almost Animated.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/Tv.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/Tv.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace scene_2 {
   [SelectionBase]
   public class Tv : shared.Tv {
@@ -18,7 +19,33 @@ namespace scene_2 {
       text_object.GetComponent<RectTransform>().offsetMin = new Vector2(10, 0);
       text_object.GetComponent<RectTransform>().offsetMax = new Vector2(-10, 0);
       /* ===================================================== */
+      text_object.GetComponent<TMPro.TextMeshProUGUI>().text =
+        "Здравствуйте господин Блок. Прошу " +
+        "прощение за то что внес сумятицу " +
+        "в ваш обычный распорядок дня. я " +
+        "понимаю как спокойно и удобно " + 
+        "однообразие, люблю его и " +
+        "наслаждаюсь им так же как и Вы. " +
+        "Но некоторые причины, которые " +
+        "естественны и неизбежны как " +
+        "восход солнца, заставляют меня " +
+        "делать то, что я делаю. Говоря " +
+        "про неизбежность, думаю Вы поняли " +
+        "кто я, и что мне надо. Слишком " +
+        "много сигарет, слишком много " +
+        "алкоголя, слишком много всего " +
+        "другого.";
+      /* ===================================================== */
+      text_object.name = GetInstanceID() + "_";
+      /* ===================================================== */
+      string[] chars = {"@", "%", "^", "&", "$"};
+      foreach (var c in chars) text_object.name += chars[UnityEngine.Random.Range(0, chars.Length)];
+      /* ===================================================== */
       GameObject.Find("Marine Mist").GetComponent<SpriteRenderer>().enabled = true;
+    }
+    
+    public void start_the_message_3() {
+      //animate text
     }
   }
 }
