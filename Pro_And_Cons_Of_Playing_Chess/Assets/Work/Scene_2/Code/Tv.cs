55561d1a45218ad681809b63f70822a3def8837f
vitalya-dev <vitalya.dev@gmail.com>
2020-07-19 17:54:43 +0300

    Animate Text.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/Tv.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/Tv.cs
@@ -6,18 +6,20 @@ using UnityEngine;
 namespace scene_2 {
   [SelectionBase]
   public class Tv : shared.Tv {
-    public void start_the_message_1() {
+    GameObject text_object;
+
+    public void message_1() {
       GameObject.FindObjectOfType<shared.SceneCamera>().zoom("1,5");
       GameObject.FindObjectOfType<shared.SceneCamera>().point_on(gameObject);
     }
 
-    public void start_the_message_2() {
+    public void message_2() {
       GameObject.Find("Tchaikovsky. 1812 Overture").GetComponent<AudioSource>().Play();
       /* ===================================================== */
-      GameObject text_object = Instantiate(Resources.Load("Etc/UIText", typeof(GameObject))) as GameObject;
+      text_object = Instantiate(Resources.Load("Etc/UIText", typeof(GameObject))) as GameObject;
       text_object.transform.SetParent(GameObject.Find("Canvas").transform);
       text_object.GetComponent<RectTransform>().offsetMin = new Vector2(10, 0);
-      text_object.GetComponent<RectTransform>().offsetMax = new Vector2(-10, 0);
+      text_object.GetComponent<RectTransform>().offsetMax = new Vector2(-10, -Screen.height);
       /* ===================================================== */
       text_object.GetComponent<TMPro.TextMeshProUGUI>().text =
         "Здравствуйте господин Блок. Прошу " +
@@ -44,8 +46,9 @@ namespace scene_2 {
       GameObject.Find("Marine Mist").GetComponent<SpriteRenderer>().enabled = true;
     }
     
-    public void start_the_message_3() {
-      //animate text
+    public void message_3() {
+      Debug.Log(Screen.height);
+      text_object.GetComponent<RectTransform>().offsetMax += new Vector2(0, 25) * Time.deltaTime;
     }
   }
 }
