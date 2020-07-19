557b96e4f261a14fd73411d27b6fa7dafdfdc52a
vitalya-dev <vitalya.dev@gmail.com>
2020-07-19 20:03:52 +0300

    Work On Camera.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Shared/Code/SceneCamera.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Shared/Code/SceneCamera.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 namespace shared {
   [SelectionBase]
   public class SceneCamera : MonoBehaviour {
+    Transform follow_target;
+    Vector3 camera_velocity;
+
+    void Update() {
+      if (follow_target) {
+        Vector3 dest = Vector3.Scale(follow_target.position, new Vector3(1, 0, 1)) + new Vector3(0, 20, 0);
+        transform.position = Vector3.SmoothDamp(transform.position, dest, ref camera_velocity, 0.25f);
+      }
+    }
+
     public void zoom(string z) {
       GetComponent<Camera>().orthographicSize = float.Parse(z);
     }
@@ -19,6 +29,14 @@ namespace shared {
       point_on(target.name);
     }
 
+
+    public void follow(string target) {
+      follow_target = GameObject.Find(target).transform;
+    }
+
+    public void follow(GameObject target) {
+      follow(target.name);
+    }
   }
 }
 
