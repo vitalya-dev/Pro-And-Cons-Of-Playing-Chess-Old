6df28bfaf177a10c23361f3355cc5b9e10ad3b18
vitalya-dev <vitalya.dev@gmail.com>
2020-08-02 17:32:29 +0300

    Got Turn.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessGame.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessGame.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 namespace scene_2 {
   static class ChessGame {
+    
+    static int turn = 1;
+
     public static int[,] board = new int [,] {
       {0, 0, 0, 0, 0, 0, 0, 0}, 
       {0, 0, 0, 0, 0, 0, 0, 0},
@@ -20,21 +23,23 @@ namespace scene_2 {
       int x1 = (int) from.x; int y1 = (int) from.y;
       int x2 = (int) to.x;   int y2 = (int) to.y;
       /* ========= */
-      if (board[y1, x1] == 0)
+      if (board[y1, x1] <= 0 || turn != 1)
         return false;
       if (board[y1, x1] == 1)
-        return pawn_move(from, to);
+        return white_pawn_move(from, to);
       else
         return false;
     }
 
-    private static bool pawn_move(Vector2 from, Vector2 to) {
+    private static bool white_pawn_move(Vector2 from, Vector2 to) {
       int x1 = (int) from.x; int y1 = (int) from.y;
       int x2 = (int) to.x;   int y2 = (int) to.y;
       /* ========= */
       if ((y1 - y2) == 1 && (x1 - x2) == 0) {
         board[y2, x2] = board[y1, x1];
         board[y1, x1] = 0;
+        /* ========= */
+        turn = -1;
         /* ========= */
         return true;
       } else {
