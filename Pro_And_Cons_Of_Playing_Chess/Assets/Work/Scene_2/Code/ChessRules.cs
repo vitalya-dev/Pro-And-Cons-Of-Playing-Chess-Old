9d46fdb470288f9c1c4b9d5f34135ba080b03884
vitalya-dev <vitalya.dev@gmail.com>
2020-08-02 14:41:01 +0300

    Pawn Moves.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessRules.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessRules.cs
@@ -12,11 +12,24 @@ static class ChessRules {
     /* ========= */
     if (board[y1, x1] == 0)
       return false;
-    /* ========= */
-    board[y2, x2] = board[y1, x1];
-    board[y1, x1] = 0;
-    /* ===================================================== */
-    return true;
+    if (board[y1, x1] == 1)
+      return pawn_move(board, from, to);
+    else
+      return false;
   }
 
+  private static bool pawn_move(int[,] board, Vector2 from, Vector2 to) {
+    int x1 = (int) from.x; int y1 = (int) from.y;
+    int x2 = (int) to.x;   int y2 = (int) to.y;
+    /* ========= */
+    if ((y1 - y2) == 1 && (x1 - x2) == 0) {
+      board[y2, x2] = board[y1, x1];
+      board[y1, x1] = 0;
+      /* ========= */
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
+
