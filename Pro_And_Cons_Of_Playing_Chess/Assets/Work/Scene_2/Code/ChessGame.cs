c6e43c0f6122aed3f9c4bf890fe9a9033f8bfc4d
vitalya-dev <vitalya.dev@gmail.com>
2020-08-02 17:21:20 +0300

    Board In ChessGame.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessGame.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessGame.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 
 namespace scene_2 {
   static class ChessGame {
-    public static bool white_move(int[,] board, Vector2 from, Vector2 to) {
+    public static int[,] board = new int [,] {
+      {0, 0, 0, 0, 0, 0, 0, 0}, 
+      {0, 0, 0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0, 0, 0}, 
+      {0, 0, 0, 0, 0, 0, 0, 0}, 
+      {0, 0, 0, 0, 0, 0, 0, 0}, 
+      {0, 0, 0, 0, 0, 0, 0, 0}, 
+      {0, 0, 0, 0, 0, 0, 1, 0}, 
+      {1, 0, 0, 0, 0, 0, 0, 0}, 
+    };
+
+    public static bool white_move(Vector2 from, Vector2 to) {
       int x1 = (int) from.x; int y1 = (int) from.y;
       int x2 = (int) to.x;   int y2 = (int) to.y;
       /* ========= */
       if (board[y1, x1] == 0)
         return false;
       if (board[y1, x1] == 1)
-        return pawn_move(board, from, to);
+        return pawn_move(from, to);
       else
         return false;
     }
 
-    private static bool pawn_move(int[,] board, Vector2 from, Vector2 to) {
+    private static bool pawn_move(Vector2 from, Vector2 to) {
       int x1 = (int) from.x; int y1 = (int) from.y;
       int x2 = (int) to.x;   int y2 = (int) to.y;
       /* ========= */
