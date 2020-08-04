ab6f0fe87f007ffef4deccdbbea99c837adbcfc2
vitalya-dev <vitalya.dev@gmail.com>
2020-08-04 09:30:17 +0300

    Working On Black Pawn Move.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessMaster.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessMaster.cs
@@ -15,8 +15,16 @@ namespace scene_2 {
     }
 
     void Update() {
-      if (chess.turn == -1)
+      if (chess.turn == -1) {
         Debug.Log("My Turn");
+        for (int i = 0; i < 8; i++) {
+          for (int j = 0; j < 8; j++) {
+            if (chess.board[i,j] != -1) {
+              chess.move(new Vector2(j, i), new Vector2(j, i + 1), -1);
+            }
+          }
+        }
+      }
     }
   }
 }
