0797134368e9f276cc9e0a15f9e276a7462243b2
vitalya-dev <vitalya.dev@gmail.com>
2020-08-09 15:00:12 +0300

    Chess Master Is Thinking Now.

--- Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessMaster.cs
+++ Pro_And_Cons_Of_Playing_Chess/Assets/Work/Scene_2/Code/ChessMaster.cs
@@ -14,17 +14,24 @@ namespace scene_2 {
       chess = GameObject.FindObjectOfType<Chess>();
     }
 
+    string state = "WAIT";
     void Update() {
-      if (chess.turn == -1) {
-        Debug.Log("My Turn");
-        for (int i = 0; i < 8; i++) {
-          for (int j = 0; j < 8; j++) {
-            if (chess.board[i,j] == -1) {
-              chess.move(new Vector2(j, i), new Vector2(j, i + 1), -1);
-            }
+      if (state == "WAIT" && chess.turn == -1) {
+        Invoke("think", 2);
+        state = "THINK";
+      }
+    }
+
+    void think() {
+      Debug.Log("My Turn");
+      for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+          if (chess.board[i,j] == -1) {
+            chess.move(new Vector2(j, i), new Vector2(j, i + 1), -1);
           }
         }
       }
+      state = "WAIT";
     }
   }
 }
